8ab4ebce8b1dfabe5bb9b2c2404e7e839b2f8032
gmonserratt <guadalupemonserratt@gmail.com>
2023-04-18 15:51:56 -0600

    Prueba commit

--- App_Code/Service.cs
+++ App_Code/Service.cs
@@ -6,7 +6,7 @@ using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
 
-//Prueba gitHub
+//Prueba gitHub II
 // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service" in code, svc and config file together.
 public class Service : IService
 {
